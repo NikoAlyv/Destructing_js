ece53ebead6c17f88ba682a3f60850dbdc0b5740
NikoAlyv <enihad801@gmail.com>
2024-01-19 06:27:17 +0400

    task1

---
+++ app.js
@@ -0,0 +1,33 @@
+//TODO task1
+//!Part 1
+const studentProfile={
+//     name:'Nihad',
+//     age:19,
+//     course:'React-native',
+//     hobbies:['Read the book','Sports']
+//  }
+ 
+ // delete studentProfile.course;
+ // console.log(studentProfile);
+ 
+ 
+ //!Part2
+ 
+ // const {name:studentName,age:studentAge,...rest}=studentProfile
+ // console.log(studentAge,studentName,rest)
+ 
+ 
+ //!Part3
+ // const grades =[85,92,78,95]
+ // console.log(grades[0],grades[1]);
+ 
+ //!Part4
+ 
+//  const additionalInfo ={
+//     adrress:'Astara',
+//     phoneNumber:'0775329507'
+//  }
+ 
+//  const complateProfile = {...studentProfile,...additionalInfo}
+//  console.log(complateProfile);
+ 
